89f4df03278779bdf5170ceedfcc7a1d9fbb5fc6
James Stanley <james@incoherency.co.uk>
2015-07-17 23:18:13 +0100

    Add words library

---
+++ lib/Cntdn/Words.pm
@@ -0,0 +1,66 @@
+package Cntdn::Words;
+
+use strict;
+use warnings;
+
+use JSON::XS qw(decode_json);
+
+sub new {
+    my ($pkg, %opts) = @_;
+
+    my $self = bless \%opts, $pkg;
+    $self->load;
+
+    return $self;
+}
+
+sub load {
+    my ($self) = @_;
+
+    die "no dictionary file specified" unless $self->{file};
+
+    open(my $fh, '<', $self->{file})
+        or die "can't read $self->{file}: $!\n";
+
+    my $i = 0;
+
+    $self->{trie} = {};
+    while (<$fh>) {
+        chomp;
+        $self->add_word($_);
+        use Data::Dumper;
+        print Dumper($self->{trie}) if $i++ == 3;
+    }
+
+    return;
+}
+
+sub add_word {
+    my ($self, $word) = @_;
+
+    my $n = $self->{trie};
+
+    my @chars = split //, $word;
+    for my $c (@chars) {
+        $n->{$c} ||= {};
+        $n = $n->{$c};
+    }
+
+    $n->{'$'} = 1;
+}
+
+sub is_word {
+    my ($self, $word) = @_;
+
+    my $n = $self->{trie};
+
+    my @chars = split //, $word;
+    for my $c (@chars) {
+        return 0 if !$n->{$c};
+        $n = $n->{$c};
+    }
+
+    return $n->{'$'};
+}
+
+1;
